0925b1b659384ac73f2d9c1398c07fdb32b93f6f
Eremia Laurentiu <eremia.laurentiu92@gmail.com>
2019-04-08 14:19:49 +0300

    New Commit

--- NumberGuessingGame/NumberGuessingGame/Program.cs
+++ NumberGuessingGame/NumberGuessingGame/Program.cs
@@ -11,7 +11,51 @@ namespace NumberGuessingGame
         static void Main(string[] args)
         {
 
-            string name;
+            //Number Guessing Game
+
+            //Instantiating a random number
+            Random rand = new Random();
+            int number = rand.Next(1, 11);
+
+            Console.WriteLine("Welcome to number guessing game\nTell me your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hi {0}", name);
+            Console.WriteLine("Guess a number between 1 and 10");
+
+            //Treating exception
+            try
+            {
+                while (true)
+                {
+                    //User input
+                    int userGuess = Convert.ToInt32(Console.ReadLine());
+
+                    if (userGuess < number)
+                    {
+                        Console.WriteLine("Guess Higher");
+                    }
+                    else if (userGuess > number)
+                    {
+                        Console.WriteLine("Guess Lower");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("You guessed it!");
+                        break;
+                    }
+
+                }
+            }
+            catch (System.FormatException ex)
+            {
+                Console.WriteLine("That is not a number!", ex.Message);
+               
+            }
+
+               
         }
+
+        
     }
 }
